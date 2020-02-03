caf58d3d37fe8581a32a3037ed5cbb198f26e411
Zac Uslianer <zacuslianer@gmail.com>
2020-02-03 22:49:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -75,7 +75,7 @@ def monopoly_with_fourth_tier
       :names =>
       {
         :reading_railroad => {
-          "mortgage value" => "$100"
+          "mortgage_value" => "$100"
         },
         :pennsylvania_railroad => {
           
